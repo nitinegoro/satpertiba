6e4cec86b2290f623cd863c070ac9efe76be18ad
Nitinegoro <pkpvicky@gmail.com>
2017-09-19 04:18:20 +0700

    First on github

---
+++ app/src/main/java/id/ac/stiepertiba/pertiba/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.stiepertiba.pertiba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nitinegoro on 9/17/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/id/ac/stiepertiba/pertiba/config/Server.java
@@ -0,0 +1,9 @@
+package id.ac.stiepertiba.pertiba.config;
+
+/**
+ * Created by nitinegoro on 9/17/2017.
+ */
+
+public class Server {
+    public static final String get_login = "http://192.168.43.241/siakad-terpadu/api/apps/login";
+}
